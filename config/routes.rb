527b4cf7dcc23e463417db116a749a7bfb078a3c
Igorfn <ifeshun@gmail.com>
2016-07-02 00:11:04 +0300

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :hotels
+  get "static_pages/help", as: "help"
+  get "static_pages/about", as: "about"
+  get "static_pages/contact", as: "contact"
   root 'hotels#index'
 
 end
