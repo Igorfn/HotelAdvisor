415e058e9549bb711a5ed162048969ac7d5fe430
Igorfn <ifeshun@gmail.com>
2016-07-01 23:10:15 +0300

    Add image uploader

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,9 @@
 class Hotel < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
+  validates :image, presence: true
+  validates :room_description,  presence: true, length: {minimum: 5, maximum: 1000};
+  validates :price_of_room, presence: true, numericality: {greater_than: 0}
+  validates :rating, numericality: {greater_than: 0, less_than_or_equal_to: 5}
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
+  validates :address, presence: true, length: {minimum: 3, maximum: 100}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701194023) do
+ActiveRecord::Schema.define(version: 20160701200337) do
 
   create_table "hotels", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160701194023) do
     t.integer  "rating"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "image"
   end
 
 end
