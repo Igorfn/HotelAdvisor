933495d07ed129b1a24e2c61b60cbc761f6598d1
Волощук Александр <ai_voloshuk@mail.ru>
2016-07-07 01:00:59 +0300

    footer

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -3,7 +3,7 @@ class HotelsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
 
   def index
-    @hotels = Hotel.page(params[:page]).per(1)
+    @hotels = Hotel.page(params[:page]).per(3)
   end
 
   def new
